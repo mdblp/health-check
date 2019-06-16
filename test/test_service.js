2f7fdfc715629d0f794efb81bd655aa9a25fbf6f
ydris <ydris@diabeloop.fr>
2019-06-16 12:44:59 +0200

    Add a couple of tests to assert the response body is correctly parsed
    * JSON content should be parsed
    * String content should be parsed as well
    * empty body should not cause any issue

--- test/test_service.js
+++ test/test_service.js
@@ -19,7 +19,7 @@ describe('Global health check service', () => {
     before((done) => {
         //prepare services mocking
         services.get('/auth/status').reply(200, "OK")
-                .get('/access/status').reply(200,"OK")
+                .get('/access/status').reply(200,"\"OK\"")
                 .get('/data/status').reply(200,"OK");
         svc.start(8080,done);
     });
@@ -48,12 +48,14 @@ describe('Global health check service', () => {
     });
     describe('When one service does not respond', () => {
         let statusResults = [];
-        it('the service should return an http error 503 and an array of 3 items', (done) => {
+        before(() => {
             //redefine mock urls to make one of them to fail:
             nock.cleanAll();
             services.get('/auth/status').reply(500, "cannot reach server")
                     .get('/access/status').reply(200,"OK")
                     .get('/data/status').reply(200,"OK");
+        });
+        it('the service should return an http error 503 and an array of 3 items', (done) => {
             chai.request(svc.server).get('/status').end((err,res) => {
                 res.should.have.status(503);
                 statusResults = res.body;
@@ -68,7 +70,37 @@ describe('Global health check service', () => {
             authStatus[0].error.should.eql("500 - \"cannot reach server\"");
         })
     });
-
+    describe('Body content should be correctly parsed', () => {
+        let statusResults = [];
+        const tidewhispererStatus = {status: "OK", version: "1.2.3"};
+        before(() => {
+            //redefine mock urls to make one of them to fail:
+            nock.cleanAll();
+            services.get('/auth/status').reply(200, "")
+                    .get('/access/status').reply(200,"\"OK\"")
+                    .get('/data/status').reply(200,JSON.stringify(tidewhispererStatus));
+        });
+        it('the service should return an http status 200 and an array of 3 items', (done) => {
+            chai.request(svc.server).get('/status').end((err,res) => {
+                res.should.have.status(200);
+                statusResults = res.body;
+                res.body.should.be.a('array');
+                res.body.length.should.eql(3);
+                done();
+            });
+        });
+        it('An empty body should not generate an error', () => {
+            let authStatus = statusResults.filter(status => status.service == "shoreline");          
+            authStatus[0].details.should.eql("");
+        });
+        it('JSON content should be parsed', () => {           
+            let result = statusResults.filter(status => status.service == "gatekeeper");
+            result[0].details.should.eql("OK");
+            result = statusResults.filter(status => status.service == "tide-whisperer");
+            result[0].details.should.be.an('object');
+            result[0].details.should.eql(tidewhispererStatus);
+        });
+    });
 
     after(() => {
         svc.stop();
